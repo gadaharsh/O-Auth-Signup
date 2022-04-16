501977782cc4382b57df044b527ea0fc0621eb15
gadaharsh <gada.hp@somaiya.edu>
2022-04-17 00:14:59 +0530

    navigation

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import SignUpScreen from "./app/screens/SignUpScreen";
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
 export default function App() {

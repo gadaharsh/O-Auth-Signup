ef5fcfbac650d1c6cd4651d9d675ba7fdda941ce
gadaharsh <gada.hp@somaiya.edu>
2022-04-17 01:51:05 +0530

    setting up firebase

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
-import * as firebase from "firebase";
+// import * as firebase from "firebase";
+import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,13 +15,15 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-let app;
-if (firebase.apps.length === 0) {
-  app = firebase.initializeApp(firebaseConfig);
-} else {
-  app = firebase.app();
-}
+// let app;
+// if (firebase.apps.length === 0) {
+//   app = firebase.initializeApp(firebaseConfig);
+// } else {
+//   app = firebase.app();
+// }
 
-const auth = firebase.auth()
+// const auth = firebase.auth()
 
-export {auth};
+// export {auth};
+
+const app = initializeApp(firebaseConfig);
